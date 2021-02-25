d8a615663c4df859d0ce15a4cd41c9fcfe1267cc
Chris Aquino <chris@radishmouse.com>
2021-02-25 11:25:04 -0500

    adds console based demo code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,128 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// We are importing a thing *named* createStore
+// from the redux node module.
+import { createStore } from 'redux';
+
+// State: this is your data!
+// Describe what state looks like, as a default value
+const defaultState = {
+  balance: 50
+};
+
+// Actions: these are your "withdrawal" and "deposit" slips
+// Rules of actions:
+// 1. They must be an object
+// 2. They must have a `type` key (i.e., type: 'withdrawal' or type: 'deposit')
+// (optional third rule) 3. attach any additional info under a `payload` key
+//      example: payload: { amount: 5 }
+// What kinds of changes would we want to make to state?
+// {
+//   type: 'withdraw',
+//   payload: {
+//     amount: 5
+//   }
+// }
+
+// {
+//   type: 'deposit',
+//   payload: {
+//     amount: 100000000
+//   }
+// }
+const WITHDRAW = 'withdraw';
+const DEPOSIT = 'deposit';
+// Write Action Creator functions that generate those action objects!!!!
+const deposit = (amount) => (
+  {
+    type: DEPOSIT, // not an arg, because typos!
+    payload: {
+      amount
+    }
+  }  
+);
+
+const withdraw = (amount) => (
+  {
+    type: WITHDRAW,
+    payload: {
+      amount
+    }
+  }
+)
+
+
+// Reducer: this calculates how an action changes state
+const bankTeller = (state, action) => {
+  
+  if (!action) return state;
+  if (!action.payload) return state;
+
+  let newState = {
+    ...state
+  };
+  // - access your account balance
+  //      it receives a copy of the current state
+  // - see if you're doing a withrawal or deposit
+  //      it receives the action
+  switch (action.type) {
+    case DEPOSIT:
+      newState.balance += action.payload.amount;
+      break;
+      case WITHDRAW:
+      newState.balance -= action.payload.amount;
+      break;
+    default:
+      // no change
+      break;
+  }
+
+  // - update your balance after calculating new value
+  //      it returns the new version of state!!!
+  return newState;
+}
+
+// The store: spoiler alert - redux gives you a function to create one!
+const store = createStore(bankTeller, defaultState);
+// .subscribe() - you pass it a function, it runs your function when state changes
+// .dispatch() - you pass it an action, it calls the reducer and updates state
+
+// Because we're not using React yet, we'll attach some things to the global window object:
+window.store = store;
+window.deposit = deposit;
+window.withdraw = withdraw;
+
+
+// Step two, set up automatic console.log() when the state changes
+store.subscribe(() => {
+  console.log('-------- state was updated --------');
+  console.table(store.getState());
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
